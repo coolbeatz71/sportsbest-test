0bea50a1095632d92c085cb06d0e38aa3740fcd1
coolbeatz71 <sigmacool@gmail.com>
2022-07-14 02:01:55 +0200

    feat: refactor the EventCard component

--- components/EventContainer/index.tsx
+++ components/EventContainer/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import EventCard from '@components/cards/EventCard';
 import { Heading } from '@heathmont/moon-core';
+import { Button } from '@heathmont/moon-core';
 
 const EventContainer: FC = () => {
     return (
@@ -10,6 +11,9 @@ const EventContainer: FC = () => {
             </Heading>
             <EventCard />
             <EventCard />
+            <Button size="large" color="piccolo.100" fullWidth>
+                See all featured events
+            </Button>
         </div>
     );
 };
