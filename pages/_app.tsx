3e19ca21e66635d67e8965e9e2b95df615124e7f
coolbeatz71 <sigmacool@gmail.com>
2022-07-14 05:36:17 +0200

    feat: implement the query to fetch event

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-import { ApolloProvider } from '@apollo/client';
+import { Provider } from 'urql';
 import { ThemeProvider, sportsbetDark } from '@heathmont/moon-themes';
 import client from '@graphql/config';
 
@@ -8,11 +8,11 @@ import '@styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
-        <ApolloProvider client={client}>
+        <Provider value={client}>
             <ThemeProvider theme={sportsbetDark}>
                 <Component {...pageProps} />
             </ThemeProvider>
-        </ApolloProvider>
+        </Provider>
     );
 };
 
